827a958ec73d1158adb43e02fed0c90f943f4e2c
Malakye Brown <prophetoftruth07@gmail.com>
2025-09-22 12:06:39 -0400

    Established Branch

--- Chapter2ProgrammingProject.cpp
+++ Chapter2ProgrammingProject.cpp
@@ -1,4 +1,4 @@
-// There are 3 Branches for this program that corresponding to the 3 assignments of the Chapter 2 Programming Project
+// This is the Gas Mileage portion of the Chapter 2 Programming Project
 
 #include <iostream>
 using namespace std;
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 
-    cout << "You're on the \"Master\" Branch of this project. Delve into the 3 sub-branchs to grade" << endl;
+    cout << "Gas Mileage!" << endl;
 
     return 0;
 }
