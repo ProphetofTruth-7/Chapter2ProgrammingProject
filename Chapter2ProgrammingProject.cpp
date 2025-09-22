962ad3b3e0d40ba3d9aacab8bd5879941e15a650
Malakye Brown <prophetoftruth07@gmail.com>
2025-09-22 11:57:36 -0400

    Redefined Master Branch

--- Chapter2ProgrammingProject.cpp
+++ Chapter2ProgrammingProject.cpp
@@ -1,20 +1,12 @@
-// Chapter2ProgrammingProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// There are 3 Branches for this program that corresponding to the 3 assignments of the Chapter 2 Programming Project
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    double oceanRisePerYear;
-    oceanRisePerYear = 1.5;
 
-    cout << "At it's current rate, the Ocean is deepening by 1.5 millimeters per year" << endl;
-    cout << "In 5 years, the Ocean will be " << oceanRisePerYear * 5.0 << " millimeters deeper/taller" << endl;
-    cout << "In 7 years, the Ocean will be " << oceanRisePerYear * 7.0 << " millimeters deeper/taller" << endl;
-    cout << "In 10 years, the Ocean will be " << oceanRisePerYear * 10.0 << " millimeters deeper/taller \n \n" << endl;
-
-    cout << "Space Test" << endl;
+    cout << "You're on the \"Master\" Branch of this project. Delve into the 3 sub-branchs to grade" << endl;
 
     return 0;
 }
