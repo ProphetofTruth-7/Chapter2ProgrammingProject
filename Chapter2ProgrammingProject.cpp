71de291badc00efafcbc3b45a5345360c15edc44
Malakye Brown <prophetoftruth07@gmail.com>
2025-09-22 11:46:54 -0400

    Add project files.

---
+++ Chapter2ProgrammingProject.cpp
@@ -0,0 +1,20 @@
+// Chapter2ProgrammingProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double oceanRisePerYear;
+    oceanRisePerYear = 1.5;
+
+    cout << "At it's current rate, the Ocean is deepening by 1.5 millimeters per year" << endl;
+    cout << "In 5 years, the Ocean will be " << oceanRisePerYear * 5.0 << " millimeters deeper/taller" << endl;
+    cout << "In 7 years, the Ocean will be " << oceanRisePerYear * 7.0 << " millimeters deeper/taller" << endl;
+    cout << "In 10 years, the Ocean will be " << oceanRisePerYear * 10.0 << " millimeters deeper/taller \n \n" << endl;
+
+    cout << "Space Test" << endl;
+
+    return 0;
+}
