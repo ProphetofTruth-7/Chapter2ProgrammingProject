172d436b8c03a22d7f66423529e98e96d17f50e4
Malakye Brown <prophetoftruth07@gmail.com>
2025-09-22 12:08:17 -0400

    Established the third and final branch

--- Chapter2ProgrammingProject.cpp
+++ Chapter2ProgrammingProject.cpp
@@ -1,4 +1,4 @@
-// There are 3 Branches for this program that corresponding to the 3 assignments of the Chapter 2 Programming Project
+// This is the Energy Drink portion of the Chapter 2 Programming Project
 
 #include <iostream>
 using namespace std;
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 
-    cout << "You're on the \"Master\" Branch of this project. Delve into the 3 sub-branchs to grade" << endl;
+    cout << "Energy Drink!" << endl;
 
     return 0;
 }
