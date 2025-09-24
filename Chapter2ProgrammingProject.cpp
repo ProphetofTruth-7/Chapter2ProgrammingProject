7b8b3708dfcaf36845960a65fe1a7405026c70a0
Malakye Brown <prophetoftruth07@gmail.com>
2025-09-24 12:12:30 -0400

    Completion

--- Chapter2ProgrammingProject.cpp
+++ Chapter2ProgrammingProject.cpp
@@ -1,12 +1,17 @@
 // This is the Energy Drink portion of the Chapter 2 Programming Project
 
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 
 int main()
 {
-
-    cout << "Energy Drink!" << endl;
+    const int SOFT_DRINK_CUSTOMERS = 16500;
+    double energyDrinkApostates = 16500 * 0.15;
+    int citrusEnjoyers = round(energyDrinkApostates * 0.58);
+    cout << "Of the " << SOFT_DRINK_CUSTOMERS << " customers of the Soft Drink Company, " << energyDrinkApostates << " buy their energy drinks\n" << endl;
+    cout << "Of those, " << citrusEnjoyers << " prefer the citrus flavored energy drink" << endl;
 
     return 0;
 }
