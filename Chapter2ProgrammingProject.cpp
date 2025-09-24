41b67ae021c21c80586c8a1855df0080b18dfc5b
Malakye Brown <prophetoftruth07@gmail.com>
2025-09-24 11:39:05 -0400

    Completion

--- Chapter2ProgrammingProject.cpp
+++ Chapter2ProgrammingProject.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int main()
 {
-    const int Tank = 20;
-    cout << "Gas Mileage!" << endl;
+    const int TANK = 20;
+    double townDriving = 23.5;
+    double highwayDriving = 28.9;
+
+    cout << "While driving in town with a full tank, you can travel " << townDriving*TANK << " miles" << endl;
+    cout << "While driving on the highway with a full tank, you can travel " << highwayDriving * TANK << " miles" << endl;
 
     return 0;
 }
